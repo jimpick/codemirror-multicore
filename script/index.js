868e1d95192808e84a35de3724a0c4787a5f0761
Glitch (codemirror-multicore) <none>
2018-02-26 06:06:50 +0000

    🗺🎎 Updated with Glitch - Working on indexeddb bug

--- script/index.js
+++ script/index.js
@@ -5,34 +5,51 @@ const storage = require('random-access-idb')('codemirror-multicore')
 const websocket = require('websocket-stream')
 const pump = require('pump')
 const prettyHash = require('pretty-hash')
+const toBuffer = require('to-buffer')
+const hyperdrive = require('hyperdrive')
+const hypercore = require('hypercore')
 const Editor = require('./editor')
 const Multicore = require('./multicore')
+const template = require('./template')
+
+require('events').prototype._maxListeners = 100
 
 const app = choo()
 app.use(devtools())
 app.use(store)
 app.route('/', mainView)
+app.route('/page/:key', mainView)
 app.mount('body')
 
 const editor = new Editor()
 
 function mainView (state, emit) {
   let link = html`<span class="help">Edit the HTML below, then click on "Publish" to create a new web site!</span>`
-  if (state.currentArchive) {
-    const url = `dat://${state.currentArchive.key.toString('hex')}`
+  if (state.currentArchive && state.currentArchive.metadata.key) {
+    const url = `dat://${state.currentArchive.metadata.key.toString('hex')}`
     link = html`<a href=${url}>${url}</a>`
   }
-  const optionList = Object.keys(state.archives).sort().map(
-    key => html`<option value=${key}>${prettyHash(key)}</option>`)
+  const optionList = Object.keys(state.archives).sort().map(key => {
+    let label = prettyHash(key)
+    const title = state.archives[key].title
+    if (title) {
+      label += ` ${title}`
+    }
+    return html`<option value=${key}>${label}</option>`
+  })
+  const optGroup = optionList.length > 0 ? html`
+    <optgroup label="Load">
+      ${optionList}
+    </optgroup>` : null
   return html`
     <body>
       <h2>
         Create a webpage on the Peer-to-Peer Web!
       </h2>
       <header>
-        <select name="docs">
+        <select name="docs" onchange=${selectPage}>
           <option value="new">Create a new webpage...</option>
-          ${optionList}
+          ${optGroup}
         </select>
         <div class="title">
           <span>Title:</span>
@@ -45,41 +62,72 @@ function mainView (state, emit) {
           ${link}
         </div>
       </header>
-      ${editor.render()}
+      ${editor.render(state.indexHtml)}
       <footer>
         Edit this on <a href="https://glitch.com/edit/#!/codemirror-multicore">Glitch</a>
       </footer>
     </body>
   `
+  
+  function selectPage (e) {
+    const key = e.target.value
+    emit('pushState', `/page/${key}`)
+  }
 }
 
 function store (state, emitter) {
   state.archives = {}
   state.currentArchive = null
-  state.title = 'My Dat Page'
+  state.indexHtml = ''
+  state.title = ''
 
   const multicore = new Multicore(storage)
   multicore.ready(() => {
     const archiverKey = multicore.archiver.changes.key.toString('hex')
+
     emitter.on('publish', () => {
       const archive = state.currentArchive ? state.currentArchive :
         multicore.createArchive()
       console.log('Archiver key:', archiverKey)
       const value = editor.codemirror.getValue()
       archive.ready(() => {
-        console.log('Key:', archive.key.toString('hex'))
-        archive.writeFile('/index.html', value, err => {
+        const key = archive.metadata.key.toString('hex')
+        console.log('Key:', key)
+        console.log('Secret Key:', archive.metadata.secretKey)
+        setTimeout(() => {
+          readSecretKey(archive.metadata.discoveryKey.toString('hex'), () => {
+            console.log('Jim publish secretKey read')
+          })
+        }, 2000)
+        const datJson = {
+          url: `dat://${key}/`,
+          title: document.getElementById('title').value,
+          description: ''
+        }
+        archive.writeFile('/dat.json', JSON.stringify(datJson, null, 2), err => {
           if (err) {
             console.error('Error writing to Dat', err)
             return
           }
-          console.log('Success.')
-          state.currentArchive = archive
-          emitter.emit('render')
+          archive.writeFile('/index.html', value, err => {
+            if (err) {
+              console.error('Error writing to Dat', err)
+              return
+            }
+            console.log('Success.')
+            state.currentArchive = archive
+            emitter.emit('pushState', `/page/${key}`)
+            multicore.replicateFeed(archive.metadata)
+          })
         })
-        multicore.replicateFeed(archive)
       })
     })
+
+    emitter.on('navigate', () => {
+      console.log('Jim navigate', state)
+      updateDoc()
+    })
+    
     const host = document.location.host
     const url = `wss://${host}/archiver/${archiverKey}`
     const stream = websocket(url)
@@ -88,12 +136,113 @@ function store (state, emitter) {
       multicore.archiver.replicate({encrypt: false}),
       stream
     )
+    multicore.archiver.on('add-archive', readMetadata)
     Object.keys(multicore.archiver.archives).forEach(dk => {
       const archive = multicore.archiver.archives[dk]
-      const key = archive.metadata.key.toString('hex')
-      state.archives[key] = dk
+      readMetadata(archive.metadata, archive.content)
     })
-    emitter.emit('render')
+    updateDoc()
+    
+    function updateDoc () {
+      if (!state.params.key) {
+        console.log('Jim reset doc')
+        state.title = 'My Dat Page'
+        state.indexHtml = template
+        emitter.emit('render')
+      } else {
+        const key = state.params.key
+        if (state.currentArchive && state.currentArchive.metadata.key.toString('hex') === key) return
+        let archive
+        if (state.archives[key] && state.archives[key].archive) {
+          archive = state.archives[key].archive
+          console.log('Key found (cached)', key)
+        } else {
+          const dk = hypercore.discoveryKey(toBuffer(key, 'hex')).toString('hex')
+          if (multicore.archiver.archives[dk]) {
+            const {metadata, content} = multicore.archiver.archives[dk]
+            readSecretKey(dk, () => { console.log('Jim read secret key done.') })
+            const options = {
+              metadata,
+              content,
+              sparse: true,
+              sparseMetadata: true
+            }
+            archive = new hyperdrive(null, key, options)
+            state.archives[key].dk = dk
+            state.archives[key].archive = archive
+            console.log('Key found (loaded)', key)
+          } else {
+            console.error('Key not found locally', key)
+            // FIXME: Throw
+            return
+          }
+        }
+        archive.readFile('index.html', 'utf-8', (err, data) => {
+          if (err) {
+            console.error('Error reading index.html', key, err)
+            return
+          }
+          try {
+            state.indexHtml = data
+            state.currentArchive = archive
+            emitter.emit('render')
+          } catch(e) {
+            // FIXME: Throw an error to the UI
+          }
+        })
+      }
+    }
+    
+    function readMetadata (metadata, content) {
+      const key = metadata.key.toString('hex')
+      const dk = metadata.discoveryKey.toString('hex')
+      state.archives[key] = {
+        dk
+      }
+      emitter.emit('render')
+      let archive
+      if (state.archives[key].archive) {
+        archive = state.archives[key].archive
+      } else {
+        readSecretKey(dk, () => { console.log('Jim read secret key done.') })
+        const options = {
+          metadata,
+          content,
+          sparse: true,
+          sparseMetadata: true
+        }
+        archive = new hyperdrive(null, key, options)
+        state.archives[key].archive = archive
+      }
+      archive.readFile('dat.json', 'utf-8', (err, data) => {
+        if (err) {
+          console.error('Error reading dat.json', key, err)
+          return
+        }
+        try {
+          const {title} = JSON.parse(data.toString())
+          state.archives[key].title = title
+          if (state.params.key === key) state.title = title
+          emitter.emit('render')
+        } catch(e) {
+          // Don't worry about it
+        }
+      })
+    }
+    
+    function readSecretKey (dk, cb) {
+      // const path = 'feeds/' + dk.slice(0, 2) + '/' + dk.slice(2, 4) + '/' + dk.slice(4) + '/secret_key'
+      const path = 'feeds/' + dk.slice(0, 2) + '/' + dk.slice(2, 4) + '/' + dk.slice(4) + '/metadata/secret_key'
+      const privateKeyFile = storage(path)
+      privateKeyFile.read(0, 64, (err, data) => {
+        if (err) {
+          console.error('readSecretKey error', path, err)
+          return cb(err)
+        }
+        console.log('Jim secret key', data)
+        cb(null, data)
+      })
+    }
   })
 }
 
